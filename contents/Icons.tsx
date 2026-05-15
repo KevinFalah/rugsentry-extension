a70b3ea65d12227ea9719d94fbfde2cca3c075d1
KevinFalah <kevinghossanfalah34@gmail.com>
2026-05-15 14:53:33 +0700

    fix <all_urls> leak issue

--- contents/Icons.tsx
+++ contents/Icons.tsx
@@ -1,4 +1,13 @@
+import type { PlasmoCSConfig } from "plasmo"
 import React from "react"
+
+export const config: PlasmoCSConfig = {
+  matches: [
+    "https://*.dexscreener.com/*",
+    "https://*.birdeye.so/*",
+    "https://*.pump.fun/*"
+  ]
+}
 
 export const CheckCircleIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
--- contents/style.ts
+++ contents/style.ts
@@ -1,4 +1,13 @@
+import type { PlasmoCSConfig } from "plasmo"
 import styleText from "data-text:~/style.css"
+
+export const config: PlasmoCSConfig = {
+  matches: [
+    "https://*.dexscreener.com/*",
+    "https://*.birdeye.so/*",
+    "https://*.pump.fun/*"
+  ]
+}
 
 export const getStyle = () => {
   const style = document.createElement("style")
